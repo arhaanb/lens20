ccaeedb8f2a61e0f56b45a9f4fb48db3f1d42293
arhaanb <arhaanb@gmail.com>
2020-09-21 19:03:20 +0530

    basic styles

---
+++ src/layouts/Default.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="layout">
+    <div class="sidenav">
+      <div class="container">
+        <div class="promo">
+          <h3>MINET Lens</h3>
+          <p>Recruitments</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="main">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+}
+.layout {
+  font-family: "inter";
+}
+
+.sidenav {
+  height: 100%;
+  width: 45vw;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  overflow-x: hidden;
+  background-image: url("../0409.jpeg");
+  background-size: cover;
+  background-position: center;
+  .promo {
+    height: 80vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    // background-color: rgba(0, 0, 0, 0.637);
+    color: #fff;
+  }
+}
+
+.main {
+  margin-left: 45vw; /* Same as the width of the sidenav */
+  padding: 2em 0;
+}
+
+@media (max-width: 750px) {
+  .sidenav {
+    width: 100%;
+    z-index: -2;
+    height: 70vh;
+    background-size: contain;
+    background-repeat: no-repeat;
+    margin-top: 1.5em;
+    .promo {
+      height: auto;
+    }
+  }
+  .main {
+    margin: 0;
+    margin-top: 80vh;
+    background-color: #fff;
+  }
+}
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,58 @@
+<template>
+  <Layout>
+    <div class="container">
+      <h2>MINET Lens</h2>
+      <p>
+        <a href="//minet.co">MINET Lens</a>
+        is the official Image component for use in building Gatsby websites. It
+        provides the fastest, most optimized image loading performance possible
+        for Gatsby (and other React) websites.
+      </p>
+      <p>
+        The component requires
+        <strong>no configuration</strong>
+        when used within Gatsby. All image processing is done within Gatsby and
+        official plugins.
+      </p>
+      <p>
+        See the
+        <a href="https://www.gatsbyjs.org/">component’s documentation</a>
+        as well as
+        <a href="https://github.com/arhaanb/">view this site’s source</a>
+        to learn how to start using gatsby-image on your Gatsby sites.
+      </p>
+      <h4>Out of the box it:</h4>
+      <ul>
+        <li>
+          Loads the optimal size of image for each device size and screen
+          resolution
+        </li>
+        <li>
+          Holds the image position while loading so your page doesn’t jump around
+          as images load
+        </li>
+        <li>
+          Uses the “blur-up” effect i.e. it loads a tiny version of the image to
+          show while the full image is loading
+        </li>
+        <li>Alternatively provides a “traced placeholder” SVG of the image.</li>
+        <li>
+          Lazy loads images which reduces bandwidth and speeds the initial load
+          time
+        </li>
+        <li>Uses WebP images if browser supports the format</li>
+      </ul>
+    </div>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Home",
+  },
+};
+</script>
+
+<style>
+</style>
