1b93fffd4bcdfc58614b3f32d803da59a62becbd
arhaanb <arhaanb@gmail.com>
2020-10-05 23:24:08 +0530

    fix deployment

--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
-// This is the main.js file. Import global CSS and scripts here.
-// The Client API can be used here. Learn more: gridsome.org/docs/client-api
-
 import DefaultLayout from '~/layouts/Default.vue'
-import './assets/css/skeleton.css'
-import './assets/css/global.css'
+import '~/assets/css/skeleton.css'
+import '~/assets/css/global.css'
 
 export default function (Vue, { router, head, isClient }) {
 	head.meta.push({
