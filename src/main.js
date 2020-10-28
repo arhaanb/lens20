9a1945d4d9c230ef53df2533605a1c519f7e6c78
arhaanb <arhaanb@gmail.com>
2020-10-28 20:10:20 +0530

    seo

--- src/main.js
+++ src/main.js
@@ -8,12 +8,17 @@ import './global.css'
 export default function (Vue, { router, head, isClient }) {
 	head.meta.push({
 		name: 'keywords',
-		content: "MINET, Lens, MIS, Mother's, Mothers, Mother, International, School"
+		content: "MINET, Lens, MIS, Mother's, Mothers, Mother, International, School, Photography, X, 2020, MINETX"
 	})
 
 	head.meta.push({
 		name: 'theme-color',
 		content: "#6699ee"
+	})
+
+	head.meta.push({
+		name: 'og:image',
+		content: 'https://minet.co/lens/lens.svg'
 	})
 
 	// Set default layout as a global component
